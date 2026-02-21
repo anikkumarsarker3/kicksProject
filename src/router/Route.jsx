419ec2622616740c3b73d16e00342107f776e5f0
Anik Kumar <anikk0388@gmail.com>
2026-02-21 11:32:14 +0600

    deployment issuee trying to fix

--- src/router/Route.jsx
+++ src/router/Route.jsx
@@ -27,6 +27,10 @@ export const router = createBrowserRouter([
                 path: "/cart",
                 element: <CartPage />
             },
+            {
+                path: "*",
+                element: <ErrorPage />
+            },
         ]
     }
 ])
